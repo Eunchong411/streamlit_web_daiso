d334436f92cf673c8e11bb8cd40b9cfab15331ec
Eunchong411 <124109440+Eunchong411@users.noreply.github.com>
2023-03-23 17:04:21 +0900

    Update 2_행정구별 신고빈도.py

--- pages/2_행정구별 신고빈도.py
+++ pages/2_행정구별 신고빈도.py
@@ -9,7 +9,7 @@ plt.rcParams['axes.unicode_minus'] = False
 if platform.system() == 'Darwin':  # 맥OS
     rc('font', family='AppleGothic')
 elif platform.system() == 'Windows':  # 윈도우
-    path = "c:/Windows/Fonts/NanumGothic.ttf"
+    path = "c:/Windows/Fonts/malgun.ttf"
     font_name = font_manager.FontProperties(fname=path).get_name()
     rc('font', family=font_name)
 else:
